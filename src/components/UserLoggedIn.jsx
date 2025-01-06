3a99256dff3aad4c2526c064573833a0f2c3b0bc
Gaurav Kumar <gk44035@gmail.com>
2025-01-06 17:52:42 +0530

    package,UserLoggedIn,Signin

---
+++ src/components/UserLoggedIn.jsx
@@ -0,0 +1,111 @@
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { axiosInstance } from "../utils/axiosConfig";
+import UserAuthContext from "../context/UserAuthContext";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+const signinValidation = Yup.object({
+  email: Yup.string().required("Email is required").email("Invalid Email"),
+  password: Yup.string().required("Password is required").min(8).max(16),
+});
+const UserLoggedIn = (props) => {
+  const { userType } = props;
+  const navigate = useNavigate();
+  const { setUser } = useContext(UserAuthContext);
+  const handleSubmit = async (values) => {
+    console.log(values, "values");
+    const { email, password } = values;
+    try {
+      const response = await axiosInstance.post("api/auth/authLogin", {
+        email,
+        password,
+        type: userType,
+      });
+      console.log(response.data.user, "response from admin");
+      setUser(response.data.user);
+      toast.success("Logged in successfully");
+
+      // Navigate based on the user role
+      if (userType === "admin") {
+        navigate("/admin/allAppointment");
+      } else if (userType === "doctor") {
+        navigate("/doctor/allAppointment");
+      } else {
+        navigate("/patient/allAppointment");
+      }
+    } catch (error) {
+      toast.error("Authentication failed");
+      console.error(error);
+    }
+  };
+  return (
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+        type: userType,
+      }}
+      validationSchema={signinValidation}
+      onSubmit={handleSubmit}
+    >
+      <Form className="space-y-4 md:space-y-6">
+        <div>
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 "
+          >
+            Your email
+          </label>
+          <Field
+            type="email"
+            name="email"
+            id="email"
+            className={`bg-gray-50 border ${"border-gray-300"}  text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+            placeholder="name@company.com"
+          />
+          <ErrorMessage name="email" component="div" className="text-red-600" />
+        </div>
+
+        <div>
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 "
+          >
+            Password
+          </label>
+          <Field
+            type="password"
+            name="password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="••••••••"
+          />
+          <ErrorMessage
+            name="password"
+            component="div"
+            className="text-red-600"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        >
+          Signin
+        </button>
+        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+          Don't have an account?{" "}
+          <a
+            href="/"
+            className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+          >
+            Register here
+          </a>
+        </p>
+      </Form>
+    </Formik>
+  );
+};
+
+export default UserLoggedIn;
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import UserLoggedIn from "../components/UserLoggedIn";
+
+const Signin = () => {
+  const [userType, SetUserType] = useState(null);
+
+  return (
+    <section className="bg-gray-100  w-full h-screen ">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-[90vh] lg:h-[90vh] lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+              {"patient".match(userType)
+                ? "Create Appointment"
+                : "Access an account"}
+            </h1>
+            {!userType ? (
+              <div className="flex space-x-2">
+                <button
+                  onClick={() => SetUserType("patient")}
+                  className="w-[8rem] border-2 border-black h-[3rem] rounded-md text-center p-2 text-white bg-gray-900 font-bold"
+                >
+                  Patient
+                </button>
+                <button
+                  onClick={() => SetUserType("doctor")}
+                  className="w-[8rem] border-2 border-black h-[3rem] rounded-md text-center p-2 text-white bg-gray-900 font-bold"
+                >
+                  Therapist
+                </button>
+                <button
+                  onClick={() => SetUserType("admin")}
+                  className="w-[8rem] border-2 border-black h-[3rem] rounded-md text-center p-2 text-white bg-gray-900 font-bold"
+                >
+                  Admin
+                </button>
+              </div>
+            ) : (
+              <UserLoggedIn userType={userType} />
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Signin;
