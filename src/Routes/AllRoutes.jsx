d2505e05b5db675dfb5e96944618802b54f29a40
Gaurav Kumar <gk44035@gmail.com>
2025-01-08 22:29:55 +0530

    sidebar and routes

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -2,12 +2,50 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Signup from "../pages/Signup";
 import Signin from "../pages/Signin";
+import PatientDetails from "../pages/Admin/PatientDetails";
+import DoctorDetails from "../pages/Admin/DoctorDetails";
+import PatientList from "../pages/Admin/PatientList";
+import DoctorsList from "../pages/Admin/DoctorsList";
+import AllAppointmentsDoctor from "../pages/Doctor/AllAppointmentsDoctor";
+import ManageAvailabilityCalendar from "../pages/Doctor/ManageAvailabilityCalendar";
+import MyAvailability from "../pages/Doctor/MyAvailability";
+import AllAppointmentsPatient from "../pages/Patient/AllAppointmentsPatient";
+import MyAppointments from "../pages/Patient/MyAppointments";
+import AllAppointmentsAdmin from '../pages/Admin/AllAppointmentsAdmin';
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
+      <Route path="/admin/patientsList" element={<PatientList />}></Route>
+      <Route path="/admin/doctorsList" element={<DoctorsList />}></Route>
+      <Route
+        path="/admin/patientDetails/:id"
+        element={<PatientDetails />}
+      ></Route>
+      <Route
+        path="/admin/doctorDetails/:id"
+        element={<DoctorDetails />}
+      ></Route>
+      <Route
+        path="/admin/allAppointment"
+        element={<AllAppointmentsAdmin />}
+      ></Route>
+      <Route
+        path="/doctor/allAppointment"
+        element={<AllAppointmentsDoctor />}
+      ></Route>
+      <Route path="/doctor/myAppointmnet" element={<MyAvailability />}></Route>
+      <Route
+        path="/doctor/manageAvailability"
+        element={<ManageAvailabilityCalendar />}
+      ></Route>
+      <Route
+        path="/patient/allAppointment"
+        element={<AllAppointmentsPatient />}
+      ></Route>
+      <Route path="/patient/myAppointmnet" element={<MyAppointments />}></Route>
     </Routes>
   );
 };
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,90 @@
+import React, { useContext, useState } from "react";
+import UserAuthContext from '../context/UserAuthContext';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Sidebar = () => {
+  const { user } = useContext(UserAuthContext);
+  const [activeItem, setActiveItem] = useState(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const menulist = {
+    patient: [
+      {
+        label: "All Appointment",
+        path: "/patient/allAppointment",
+      },
+      {
+        label: "My Appointment",
+        path: "/patient/myAppointmnet",
+      },
+    ],
+    doctor: [
+      {
+        label: "All Appointment",
+        path: "/doctor/allAppointment",
+      },
+      {
+        label: "My Appointment",
+        path: "/doctor/myAppointmnet",
+      },
+      {
+        label: "Manage Availability",
+        path: "/doctor/manageAvailability",
+      },
+    ],
+
+    admin: [
+      {
+        label: "All Appointment",
+        path: "/admin/allAppointment",
+      },
+      {
+        label: "Patient",
+        path: "/admin/patientsList",
+      },
+      {
+        label: "Doctors",
+        path: "/admin/doctorsList",
+      },
+    ],
+  };
+
+  const userMenuItems = menulist[user?.userType] || [];
+
+  const handleMenuItemClick = (e, path) => {
+    e.preventDefault();
+    setActiveItem(path);
+    navigate(path);
+  };
+  useEffect(() => {
+    // Set the active item based on the current path
+    setActiveItem(location.pathname);
+  }, [location]);
+
+  return (
+    <div className="w-[15%] border-2 border-gray-300 h-screen">
+      <div className="w-full h-[6rem] border-2 border-gray-200 p-8 ">
+        <h1 className="font-bold text-xl">MY Project</h1>
+      </div>
+      <div className="p-4 space-y-8">
+        {userMenuItems.map((item, index) => (
+          <div
+            key={index}
+            onClick={(e) => handleMenuItemClick(e, item.path)} // Call the click handler with the path
+            className={`w-full h-[4rem] p-4 rounded-lg cursor-pointer 
+              ${
+                activeItem === item.path
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-300"
+              }
+              hover:bg-blue-100`}
+          >
+            <h3>{item.label}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
