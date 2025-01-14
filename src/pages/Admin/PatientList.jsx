f02cda1fb31a27f7e14d5486c62c2c0449cba97b
Gaurav Kumar <gk44035@gmail.com>
2025-01-14 22:35:07 +0530

    patientDetails and patientList

---
+++ src/pages/Admin/PatientList.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import Pagination from "../../components/Pagination";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "../../utils/axiosConfig";
+import Sidebar from "../../components/Sidebar";
+import Navbar from "../../components/Navbar";
+const PatientList = () => {
+  const [patientList, setPatientList] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [totalNoOfPatient, setTotalNoOfPatient] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const navigate = useNavigate();
+  const fetchData = async (pageNo) => {
+    try {
+      const response = await axiosInstance.get(
+        `api/admin/showPatients/?pageNo=${pageNo}`
+      );
+      console.log(response, "response");
+      setPatientList(response.data.patients);
+      setTotalPages(response.data.noOfPages);
+      setTotalNoOfPatient(response.data.noOfPatient);
+    } catch (err) {
+      console.log(err);
+      toast.error("Error while Fetching Data");
+    }
+  };
+
+  useEffect(() => {
+    fetchData(currentPage);
+  }, [currentPage]);
+  const handlePageChange = (pageNo) => {
+    if (pageNo >= 1 && pageNo <= totalPages) {
+      fetchData(pageNo);
+      setCurrentPage(pageNo);
+    }
+  };
+  function showPatientDetails(id) {
+    navigate(`/admin/patientDetails/${id}`);
+    console.log(id, "abcd");
+  }
+
+  return (
+    <div className="w-full h-screen flex ">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+        <div className="flex-1 bg-gray-100 p-6">
+          <div className="w-full overflow-hidden ">
+            <div className="w-full h-[30%]  ">
+              <div className="w-full h-8 ">
+                <h1 className="font-bold text-3xl">Patients</h1>
+              </div>
+              <table className="w-full table-auto">
+                <thead>
+                  <tr className="h-10">
+                    <th className="font-bold p-2 text-left">sr no</th>
+                    <th className="font-bold p-2 text-left">Name</th>
+                    <th className="font-bold p-2 text-left">Email</th>
+                    <th className="font-bold p-2 text-left">Number</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {patientList.map((data, index) => (
+                    <tr
+                      key={data._id}
+                      className="border-t cursor-pointer hover:bg-slate-200"
+                      onClick={() => showPatientDetails(data._id)}
+                    >
+                      <td className="p-2">{index + 1}</td>
+                      <td className="p-2">{data.name}</td>
+                      <td className="p-2">{data.email}</td>
+                      <td className="p-2">{data.phone}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+
+              <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={handlePageChange}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PatientList;
